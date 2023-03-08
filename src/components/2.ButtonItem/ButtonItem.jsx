624775bf2571bbab9c5eefc3fcdc2ae70b3fab30
KYJ0206 <gansoo.mayo@gmail.com>
2023-03-08 10:57:14 +0900

    2023-03-08 react category

--- src/components/2.ButtonItem/ButtonItem.jsx
+++ src/components/2.ButtonItem/ButtonItem.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 export default function ButtonItem({item, onMenu}) {
-    const {category} = item;
-    // console.log('리스트 카테고리 분류',item)
     return (
         <li onClick={() => onMenu(item)}>
             {item}
